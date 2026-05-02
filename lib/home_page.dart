a0f26e42dc9e188a162b21c2664c031aa8055d15
a7med3sfour099 <a7med3sfour099@gmail.com>
2026-05-02 20:51:40 +0300

    Add logout button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaselearn/auth/login.dart';
+import 'package:firebaselearn/core/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,25 +25,34 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Home Page"),
         centerTitle: true,
+        backgroundColor: AppColors.primary,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const Login()),
+                (route) => false,
+              );
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              "Counter Value:",
-              style: TextStyle(fontSize: 20),
-            ),
+            const Text("Counter Value:", style: TextStyle(fontSize: 20)),
             const SizedBox(height: 10),
             Text(
               "$counter",
               style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: increment,
-              child: const Text("Increase"),
-            ),
+            ElevatedButton(onPressed: increment, child: const Text("Increase")),
           ],
         ),
       ),
